871837a1f4db9199d40b0fd702d6ad90667f2392
danechka2228 <d97944535@gmail.com>
2025-03-06 19:54:29 +0300

    Update classic.py

--- classic.py
+++ classic.py
@@ -1,34 +1,35 @@
 import pygame
 
 pygame.init() #запомнить
+x = 100
+y = 100
+weight = 100
+height = 70
 window_size=(300, 300)#размеры окна
 screen=pygame.display.set_mode(window_size)#создаем экран с размерами
 pygame.display.set_caption("Моя игра")#название игры
 background_color = (16, 205, 200)#cоздаем цвет фона
-screen.fill(background_color)
 clock = pygame.time.Clock()
-r = pygame.Rect(100, 100 , 100 , 70)
 font = pygame.font.SysFont('Arial', 10)
-text = font.render('Ваня', True, 0)
-screen.blit(text, (100, 70))
+text = font.render('somebody cover my!!!', True, 371)
+
 while True:
-    pygame.draw.rect(screen, 324, r)
-    clock.tick(46)# считаем фпс
+    screen.fill(background_color)
+    r = pygame.Rect(x, y, weight, height)
+    pygame.draw.rect(screen, 123456789, r)
+    screen.blit(text, (110, 125))
+    clock.tick(10)# считаем фпс
     pygame.display.update() # обнова экрана
     for event in pygame.event.get(): # ходим по событиям
+        if event.type == pygame.KEYDOWN: # если клавиша нажата
+            if event.key == pygame.K_a: # если клавиша а
+                x = x-5
+            if event.key == pygame.K_w: # если клавиша а
+                y = y-5
+            if event.key == pygame.K_s:  # если клавиша а
+                y = y + 5
+            if event.key == pygame.K_d:  # если клавиша а
+                x = x + 5
         if event.type == pygame.QUIT: # нажали на крестик
             pygame.QUIT() # вышли из игры
-
-
-
-
-
-
-
-
-
-
-
-
-
 
